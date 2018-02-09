9b1fe9a055fb7cf7b4769d2fd873e2e7a88374e4
Daniel <clancy.daniel@gmail.com>
2018-02-08 18:15:32 -0600

    adde comments

--- script.js
+++ script.js
@@ -2,33 +2,50 @@
 
 $(document).ready(function() {
 
-	var count = 1;
+	// set the count of how many cards we start with
+	var count = 0;
 
+
+	// the form is submitted
 	$('#add_card').submit(function(e) {
+		count +=1;
 		e.preventDefault();
 
+
+		// start the html for the card
 		var el = '<div class="card" id="card_' + count + '" data-show-desc="false">';
 		
-
+		// add the name
 		el += '<h2>' + $('#first_name').val() + ' ' + $('#last_name').val() + '</h2>';
 		el += '<p>Click for description</p>';
 
+
+		// append the card to the end of the cards list
 		$('.cards').append(el);
 		var id = 'card_' + count;
 
+
+		// attach the card data to the card for the toggle
 		$('#' + id).data({
 					first_name: $('#first_name').val(),
 					last_name: $('#last_name').val(),
 					description: $('#description').val(),
 				});
 
+
+		//clear the input fields
 		$('#add_card').children('label').children('input[type="text"]').val('');
 		$('#add_card').children('label').children('textarea').val('');
-		count +=1;
+		
 	});
 
+
+	// when you click on a card
 	$(document).on('click', '.card', function(){
 
+
+		// if show desc is false, show the desc.
+		// if show desc is true, show name
 		if($(this).attr('data-show-desc') == "false") {
 			$(this).attr('data-show-desc', 'true')
 			$(this).html('<p>' + $(this).data('description') + '</p>');
